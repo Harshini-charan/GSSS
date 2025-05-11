203aeea2e5b1a5759831b61260878bebfe2813a1
Harshini-charan <cs814409@gmail.com>
2025-05-11 12:23:09 +0530

    Add files via upload

---
+++ pgm1_maven/src/main/java/GSSS_Maven_pgm1/pgm1_maven/App.java
@@ -0,0 +1,13 @@
+package GSSS_Maven_pgm1.pgm1_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Inchara!" );
+    }
+}
---
+++ pgm1_maven/src/test/java/GSSS_Maven_pgm1/pgm1_maven/AppTest.java
@@ -0,0 +1,38 @@
+package GSSS_Maven_pgm1.pgm1_maven;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
